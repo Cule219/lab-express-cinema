10b24157f461b133559da4c4c4fe7469b5f79feb
Chad Hinds <chadrickj8@gmail.com>
2020-05-13 16:05:04 -0400

    Add routing

--- starter_code/client/src/App.js
+++ starter_code/client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Route exact path='/' render={(props) => <Home {...props} />}></Route>
+        </Switch>
       </div>
     );
   }
