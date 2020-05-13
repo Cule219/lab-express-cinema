2b44bdf750367a95c7299c1a60cc69675a8085ae
DavidColatti <davidcolatti@gmail.com>
2020-05-13 17:05:18 -0400

    iteration 2

--- starter_code/client/src/App.jsx
+++ starter_code/client/src/App.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
-import Home from "./components/Home";
+import React, { Component } from 'react';
+import Home from './components/Home';
+import { Switch, Route } from 'react-router-dom';
+import Movies from './components/Movies';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Home />
+				<Switch>
+					<Route exact path="/movies" render={(props) => <Movies {...props} />} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
