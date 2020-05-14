bbc8e9f007880358bbb2040ee1e1f54a44fd5ca4
Dazpel <alexxdav97@gmail.com>
2020-05-14 12:32:53 -0400

    it 4

--- starter_code/client/src/App.jsx
+++ starter_code/client/src/App.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from "react";
-import Home from "./components/Home";
+import React, { Component } from 'react';
+import Home from './components/Home';
+import Movies from './components/Movies';
+import MovieDetails from './components/MovieDetails.jsx';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/movies"
+            render={(props) => <Movies {...props} />}
+          />
+           <Route
+                  exact path="/movies/:id"
+                  component={(props) => <MovieDetails {...props} />}
+                />
+        </Switch>
       </div>
     );
   }
---
+++ starter_code/client/src/components/Movies.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'
+
+export default class Movies extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    // this is an example call to our backend
+    let res = await axios.get('http://localhost:5000/movies');
+    
+
+    this.setState({
+      movies: res.data,
+    });
+    console.log(this.state);
+  }
+
+  showMovies = () => {
+
+    return this.state.movies.map(eachMovie => {
+      return (
+      <li key={eachMovie._id}>
+          <h1>{eachMovie.title}</h1>
+          <img src={eachMovie.image} />
+          <button><Link to={`/movies/${eachMovie._id}`}>See more!</Link></button>
+      </li>
+      )
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        Movies Component
+        {this.showMovies()}
+      </div>
+    );
+  }
+}
+
