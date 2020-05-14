427b1b638541a30bc4989bca16ebd98133c31085
DavidColatti <davidcolatti@gmail.com>
2020-05-14 09:12:44 -0400

    iteration 3

--- starter_code/client/src/App.jsx
+++ starter_code/client/src/App.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
-import { Switch, Route } from 'react-router-dom';
-import Movies from './components/Movies';
+import React, { Component } from "react";
+import Home from "./components/Home";
+import {Switch, Route} from 'react-router-dom'
+import Movies from './components/Movies'
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Home />
-				<Switch>
-					<Route exact path="/movies" render={(props) => <Movies {...props} />} />
-				</Switch>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' render={props => <Home {...props}/>}/>
+          <Route exact path='/movies' render={props => <Movies {...props}/>}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- starter_code/models/Movie.js
+++ starter_code/models/Movie.js
@@ -1,15 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-	title: { type: String },
-	director: { type: String },
-	stars: { type: [ String ] },
-	image: { type: String },
-	description: { type: String },
-	showtimes: { type: [ String ] }
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
 });
 
-const Movie = mongoose.model('Movie', movieSchema);
+const movieModel = mongoose.model("Movie", movieSchema);
 
-module.exports = Movie;
+module.exports = movieModel;
+
+// let str = "string"
+// let num = 12345
+// let obj = {
+//   prop1: 1,
+//   prop2: 2,
+//   prop3: 3
+// }
+// let arr = [1,2,3,4]
+
+// function func () {
+//   console.log("hello")
+// }
+
+// module.exports = {movieModel, str, num, obj, arr, func};
