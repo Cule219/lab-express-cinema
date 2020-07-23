a80d3ee82967d3bfea2d8270037a517db63c1038
Stephen Z <mayistep@icloud.com>
2020-07-23 15:38:05 -0400

    done

--- starter_code/client/src/App.jsx
+++ starter_code/client/src/App.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    movies: []
+  }
+
+
+
+
+
+  componentDidMount = async () => {
+    let responseFromBackend = await axios.get("http://localhost:5000/moveis");
+    this.setState({
+        movies: responseFromBackend.data,
+    });
+  }
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
         <Home />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" render={props => <Movies {...props} movies={this.state.movies} />} />
+          <Route exact path="/movie/:movieId" render={props => <Movie {...props} movies={this.state.movies} />} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
